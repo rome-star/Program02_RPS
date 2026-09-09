8b36c7d38d88ca70245802f86631ecb555d5a65e
nue-moon <randomcolors444@gmail.com>
2026-09-08 20:35:29 -0400

    Initial code added

---
+++ Program02_RPS/Program02_RPS.cpp
@@ -0,0 +1,22 @@
+//Roman A, CSC 222, Rock Paper Scissors Tournament
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+const int ROCK = 1;
+const int PAPER = 2;
+const int SCISSORS = 3;
+const int QUIT = 4;
+
+const int PLAYER_WIN = 1;
+const int COMPUTER_WIN = 2;
+const int TIE = 3;
+
+int main()
+{
+    cout << "Rock, Paper, Scissors Tournament - coming soon!" << endl;
+    return 0;
+}
